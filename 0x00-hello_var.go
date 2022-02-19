dea95d1a0499fd9dc32f54eface5cb19616a2900
ChristianMartinezTech <crismartine.e@gmail.com>
2022-02-19 11:51:18 -0500

    implementing args

--- 0x00-hello_var.go
+++ 0x00-hello_var.go
@@ -9,10 +9,17 @@ import (
 func main() {
 	// var declaration
 	message := "Hello,"
-	// arg0 := os.Args[0] will be the program path
 	arg1 := os.Args[1] + "."
 	const message2 = "Have a good one."
+	number := 10
+
+	// To initialize empty var
+	// var a int
+	// var b string
+	// var c float64
+	// var d bool
 
 	fmt.Println(message, arg1, message2)
 	fmt.Println("PD: Hello World!")
+	fmt.Println(number - 5)
 }
