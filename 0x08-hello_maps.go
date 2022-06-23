be4d65eecf07897d180369c6e91dd060ee8c9e59
Christian Martinez <crismartine.e@gmail.com>
2022-06-23 12:01:45 -0500

    Working wih maps

--- 0x08-hello_maps.go
+++ 0x08-hello_maps.go
@@ -2,35 +2,40 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func price() {
+	menu := map[string]int{
+		"apple":        1,
+		"pinapple":     5,
+		"coconut":      3,
+		"strawberries": 2,
+		"grapes":       3,
+	}
+	fmt.Println(menu)
+
 	// Taking the customer's order
-	openingMsg := "Hey there! Here's today's menu:
+	openingMsg := `Hey there! Here's today's menu:
 	apple
 	pinneaple
 	coconut
 	strawberries
-	grapes"
+	grapes`
+	fmt.Println(openingMsg)
+	fmt.Println("What would you like to order?")
 
-	fmt.Println()
-	fmt-Println("What would oyu like to order?")
 	var order string
-	fmt.Scanf(&order)
+	fmt.Scanln(&order)
+	//fmt.Println("Your order is: " + order)
 
-	// Detecting if the client response is in the menu
-	var dict map[string]int = map[string]int {
-		"apple": 1,
-		"pinapple": 5,
-		"coconut": 3,
-		"strawberries": 2,
-		"grapes": 3
+	//Check if order is in the menu
+	if menu[order] != nil {
+
 	}
-
-	
+}
 
 func main() {
-
-
-	}
+	price()
 }
