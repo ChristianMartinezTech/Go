002ca58623eeb201cee6ac7a90860a9868509e4c
Christian Martinez <crismartine.e@gmail.com>
2022-07-04 09:17:49 -0500

    Working on the md file

---
+++ UnderstandingGoDocs.md
@@ -0,0 +1,3 @@
+# Understanding Go Documentnation
+
+This is a special markdown documents that will talk about how to understand the Golang Documentation.
--- calculator/calculator.go
+++ calculator/calculator.go
@@ -3,23 +3,42 @@
 package main
 
 import (
-	"os"
+	"fmt"
+	"log"
+	"strconv"
 )
 
-// this func separates and cast the input from str to int
+// Function that converts a string into int
 func operation(UsrInput string) {
-	for i := 0; i < len(UsrInput); i++ {
 
+	for i := 0; i < len(UsrInput); i++ {
+		op, err := strconv.Atoi(UsrInput)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(op)
 	}
+}
+
+func experiment() {
+	a := 2
+	b := "+"
+	c := 5
+
+	fmt.Printf("%T/n", a)
+	fmt.Printf("%T/n", b)
+	fmt.Printf("%T/n", c)
+	fmt.Println(a, b, c)
 
 }
 
-//func operation()
-
 func main() {
 	// Taking the user input and send it to operation
-	UsrInput := os.Args[1]
-	/*fmt.Println(UsrInput)
-	fmt.Printf("%T\n", UsrInput)*/
-	operation(UsrInput)
+	/*fmt.Println("Hello there! Input your operation:")
+	var UsrInput string
+	fmt.Scanf("%s", &UsrInput)
+	operation(UsrInput)*/
+
+	experiment()
+
 }
